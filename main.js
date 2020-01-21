53116022a98eb753b26e1a3d86dfd1d92b885ea5
Luyny <l.f3rn4ndes@gmail.com>
2020-01-21 20:58:55 -0300

    change canvas width

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import Game from './Game.js';
 export { game}
 
 var canvas = document.getElementById('canvas');
-    canvas.width = Math.min(screen.height, screen.width) -200
+    canvas.width = Math.min(window.innerHeight, window.innerWidth) -35
     canvas.height = canvas.width
 var score = document.getElementById('score');
 var ctx = canvas.getContext('2d');
